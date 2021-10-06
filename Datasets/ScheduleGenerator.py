4cd25f28f144fe16b3d6b03764eec5bc47552b71
Yida-Ding <ding001021@gmail.com>
2021-10-06 15:08:02 +0800

    yida1006crew

--- Datasets/ScheduleGenerator.py
+++ Datasets/ScheduleGenerator.py
@@ -45,6 +45,54 @@ class CidHelper:
         self.count+=1
         self.cid="C%d"%self.count
 
+class CrewAssignHelper:
+    def __init__(self,acfs):
+        self.acfs=acfs
+        #use multidigraph to store flights
+        self.G=nx.MultiDiGraph()
+        #input flights to the graph as edges
+        for acf in self.acfs:
+            for flt in acf.flights:
+                self.G.add_edge(flt.fromAp,flt.toAp,acf=acf.tail,obj=flt,SDT=flt.fromTime,SAT=flt.toTime,fltTime=flt.fltTime,seen=0)
+        
+        #sort edges by SDT so that crew will always start with an earlier flight
+        edges=sorted(self.G.edges(data=True),key=lambda t: t[2].get('SDT'))
+        #terminate condition: all the edges are covered by crew
+        while len(edges)!=0:
+            #pop out the currently earliest flight to start the crew path
+            cur=edges.pop(0)
+            path=[cur]
+            workTime=tmh.time(0,0)
+            
+            while cur!=None:
+                #find a feasible edge that is connected to cur edge
+                select=None
+                for temp in self.G.out_edges(cur[1],data=True):
+                    #conditions: 1)not seen yet; 2)accept 30 min minimum connection time; 3)within 10 hours working time
+                    if temp[2]['seen']==0 and temp[2]['SDT']>=cur[2]['SAT']+tmh.time(0,30) and workTime+temp[2]['fltTime']<=tmh.time(10,0):
+                        select=temp
+                        #if there is a chance to change aircraft, then possibly catch it
+                        if temp[2]['acf']!=cur[2]['acf'] and random.uniform(0,1)<0.8:
+                            break
+                
+                #update working time and path by the selected edge
+                if select!=None:
+                    workTime+=select[2]['fltTime']
+                    path.append(select)    
+                
+                #updation for the inner while loop
+                cur=select
+            
+            #crew assigned to each target flight
+            for target in path:
+                target[2]['obj'].crew=cidh.cid
+                target[2]['seen']=1
+                
+            #update the global variable crew id
+            cidh.update()
+            #remove seen edges
+            edges=[edge for edge in edges if edge[2]['seen']==0]
+
 
 class Aircraft:
     def __init__(self,tail):
@@ -57,12 +105,17 @@ class Aircraft:
         self.flights=[]
         curTime=tmh.winEnd
         
-        #the condition is used to ensure a feasible journey taken by the aircraft
-        while curTime>=tmh.winEnd:
-            #create random simple path of random length within 7
+        #if one of the flight in generated path is invalid regarding duration, pathFlag=False
+        pathFlag=True
+        #the first condition is used to ensure a feasible journey taken by the aircraft
+        while curTime>=tmh.winEnd or pathFlag==False:
+            #create random simple path of random length within range 4~6
             #may consider repeated nodes for the path in future
-            self.pathleng=random.randint(5,7)
-            self.path=next(nx.all_simple_paths(aph.apGraph,self.src,self.dest,self.pathleng))
+            self.path=[]
+            generator=nx.all_simple_paths(aph.apGraph,self.src,self.dest)
+            while len(self.path)<4 or len(self.path)>6:
+                self.path=next(generator)
+                
             #create flight instances with assigned SDT and SAT
             temp=[]
             curTime=tmh.winStart
@@ -73,28 +126,10 @@ class Aircraft:
                 flight.toTime=flight.fromTime+flight.fltTime
                 curTime=flight.toTime
                 temp.append(flight)
+                pathFlag=flight.flag
                 
             self.flights=temp
             
-        #crew assignment: partition the flights of an aircraft, for each crew team
-        #may consider a crew team operating two or more aircrafts in future
-        self.crews=[]
-        total=len(self.flights)
-        Nsplit=random.randint(0,total//2)
-        inds=sorted(random.sample(range(1,total),Nsplit))
-        for i,j in zip([0]+inds,inds+[None]):
-            target=self.flights[i:j]
-            crew=Crew(cidh.cid,target)
-            cidh.update()
-            self.crews.append(crew)
-            for f in target:
-                f.crew=crew
-        
-    def getAircraftInfo(self):
-        print("Tail:",self.tail)
-        print("Capacity:",self.cpc)
-        print(self.src,"-->",self.dest)
-        print("----------")
                 
 class Flight:
     def __init__(self,fid,fromAp,toAp,aircraft):
@@ -104,11 +139,16 @@ class Flight:
         self.dist=aph.getDistance(fromAp,toAp)
         self.aircraft=aircraft
         self.crew=None
+        self.flag=True
         
         #Estimate flight time by distance according to average speed 800km/h
         #cruise time is assumed to be 30 min shorter than flight time
         #may consider variable speed in future
         self.fltTime=tmh.time(round(self.dist/800,1),0)
+        #assume flight less than 30 min be an invalid flight
+        if self.fltTime<=tmh.time(0,30):
+            self.flag=False
+            
         self.crsTime=self.fltTime-tmh.time(0,30)
         self.fromTime=None
         self.toTime=None
@@ -116,52 +156,35 @@ class Flight:
         #Assume the occupancy rate is over 60%
         self.pax=round(self.aircraft.cpc*random.uniform(0.6,1))
         
-    def getFlightInfo(self):
-        print(self.fid)
-        print(self.fromAp,"-->",self.toAp)
-        print(self.fromTime)
-        print(self.toTime)
-        print("Pax:",self.pax)
-        print("----------")
-        
-
-class Crew:
-    def __init__(self,cid,flights):
-        self.cid=cid
-        self.flights=flights
-        #crew source airport is assumed to be the source of first flight
-        self.src=flights[0].fromAp
-        #crew destination airport is assumed to be the destination of last flight
-        self.dest=flights[-1].toAp
-        self.fids=[f.fid for f in flights]
-
-    def getCrewInfo(self):
-        print(self.cid)
-        print(self.src,"-->",self.dest)
-        print("Operate flights:",self.fids)
-        print("----------")
-
-
+                
+                    
 #init helper
-random.seed(0)
-aph=AirportHelper(20,1) # MAXAPT;seed 
-tmh=TimeHelper((5,0),(24,0)) # time window
+random.seed(6)
+aph=AirportHelper(6,1) # maximum nb of airports in the connected graph ; random seed 
+tmh=TimeHelper((5,0),(24,0)) # time window (hour, minute)
 cidh=CidHelper()
 
 #init aircrafts
-aircrafts=[Aircraft("T%d"%i) for i in range(5)] # operating aircrafts 
-flights,crews=[],[]
-for air in aircrafts:
-    flights+=air.flights
-    crews+=air.crews
+Napt=3 # nb of operating aircrafts     
+aircrafts=[Aircraft("T%d"%i) for i in range(Napt)]
+
+cah=CrewAssignHelper(aircrafts)
+flights=[]
+for a in aircrafts:
+    flights+=a.flights
     
 #generate dataframe
 labels=["Tail","Flight_ID","Crew_ID","From","To","SDT","SAT","Flight_time","Cruise_time","Distance(km)","Capacity","Pax"]
-res=np.array([[f.aircraft.tail,f.fid,f.crew.cid,f.fromAp,f.toAp,tmh.tostring(f.fromTime),tmh.tostring(f.toTime),tmh.tostring(f.fltTime),tmh.tostring(f.crsTime),f.dist,f.aircraft.cpc,f.pax] for f in flights])
-pd.DataFrame({labels[i]:res[:,i] for i in range(len(labels))}).to_csv("Schedule_ACF5.csv",index=None)
+res=np.array([[f.aircraft.tail,f.fid,f.crew,f.fromAp,f.toAp,tmh.tostring(f.fromTime),tmh.tostring(f.toTime),tmh.tostring(f.fltTime),tmh.tostring(f.crsTime),f.dist,f.aircraft.cpc,f.pax] for f in flights])
+df=pd.DataFrame({labels[i]:res[:,i] for i in range(len(labels))})
+df.to_csv("Schedule_ACF%d.csv"%Napt,index=None)
+print(df)
 
-#visualize entities(aircrafts/flights/crews)
-#for c in crews:
-#    c.getCrewInfo()
+
+
+
+
+
+
 
 
